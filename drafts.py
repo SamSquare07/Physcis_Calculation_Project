a53e8d3fd60463612303fccd2f136a2b8b9a0d82
SamSquare62442 <68989987+SamSquare62442@users.noreply.github.com>
2020-08-13 17:06:43 +0600

    Delete drafts.py

--- drafts.py
+++
@@ -1,8 +0,0 @@
-import math
-a = float(input("Enter your a : "))
-u = float(input("Enter your u : "))
-s = float(input("Enter your s : "))
-special_char = u**2 - 4*0.5*a*-s
-ans = (-u + math.sqrt(special_char)) / (2*(0.5*a))
-
-print(ans)
